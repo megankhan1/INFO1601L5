ba5d3faa1df34f999e707d5aa1727f1732d0410d
megankhan1 <megankhan500@gmail.com>
2025-03-08 01:02:04 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
-let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];// 10 elements
-
-for(let i=0; i< arr.length; i++){
-  console.log(arr[i])
-}
+function happyPrint(string){
+  console.log("😀: "+string);
+ }
+ 
+ function sadPrint(string){
+  console.log("😢: "+string);
+ }
+ 
+ //This high-order function adds 2 parameters and passes the answer to the callback
+ function add(a, b, callback){
+    let ans = a + b;
+    callback(ans);// call the callback and pass the answer to it
+ }
+ 
+ //call add passing the callbacks to it
+ 
+ add(5, 10, happyPrint);
+ add(11, 12, sadPrint);
