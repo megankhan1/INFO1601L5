0af5514e5b64b9924ef58df38021d2e344974c39
megankhan1 <megankhan500@gmail.com>
2025-03-11 10:19:08 -0400

    lab 5 completed

--- index.js
+++ index.js
@@ -111,3 +111,49 @@ function createPerson(name, height, weight) {
  ];
  
  console.log(avgBMI(people));
+
+ let bob = {
+   fname: "bob",
+   lname: "smith",
+   age: 18,
+   height: 6,
+   transcript:[
+     {
+       course: 'INFO 1603',
+       grades: [ 89, 34, 67 ]
+     },
+     {
+       course: 'INFO 1601',
+       grades: [ 89, 34, 67 ]
+     }
+   ]
+ };
+ 
+ let sally = {
+   fname: "sally",
+   lname: "smith",
+   age: 18,
+   height: 6,
+   transcript:[
+     {
+       course: 'INFO 1601',
+       grades: [ 100, 89, 79 ]
+     }
+   ]
+ };
+ 
+ let paul = {
+   fname: "paul",
+   lname: "smith",
+   age: 18,
+   height: 6,
+   transcript:[
+     {
+       course: 'INFO 1600',
+       grades: [ 89, 34, 67 ]
+     }
+   ]
+ };
+ 
+ 
+ const students = [bob, sally, paul];
